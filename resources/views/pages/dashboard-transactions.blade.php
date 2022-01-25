15af9f98cd347070cdc4c4383f74b74b4689ff15
dandisatrio <dandisatrio810@gmail.com>
2022-01-25 17:25:59 +0700

    convert dashboard transactions page

---
+++ resources/views/pages/dashboard-transactions.blade.php
@@ -0,0 +1,163 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Store Dashboard Transactions')
+
+@section('content')
+<div
+    class="section-content section-dashboard-home"
+    data-aos="fade-up"
+    >
+    <div class="container-fluid">
+        <div class="dashboard-heading">
+        <h2 class="dashboard-title">Transactions</h2>
+        <p class="dashboard-subtitle">
+            Big result start from the small one
+        </p>
+        </div>
+        <div class="dashboard-content">
+        <div class="row">
+            <div class="col-12 mt-2">
+            <ul
+                class="nav nav-pills mb-3"
+                id="pills-tab"
+                role="tablist"
+            >
+                <li class="nav-item" role="presentation">
+                <button
+                    class="nav-link active"
+                    id="pills-home-tab"
+                    data-bs-toggle="pill"
+                    data-bs-target="#pills-home"
+                    type="button"
+                    role="tab"
+                    aria-controls="pills-home"
+                    aria-selected="true"
+                >
+                    Sell Product
+                </button>
+                </li>
+                <li class="nav-item" role="presentation">
+                <button
+                    class="nav-link"
+                    id="pills-profile-tab"
+                    data-bs-toggle="pill"
+                    data-bs-target="#pills-profile"
+                    type="button"
+                    role="tab"
+                    aria-controls="pills-profile"
+                    aria-selected="false"
+                >
+                    Buy Product
+                </button>
+                </li>
+            </ul>
+            <div class="tab-content" id="pills-tabContent">
+                <div
+                class="tab-pane fade show active"
+                id="pills-home"
+                role="tabpanel"
+                aria-labelledby="pills-home-tab"
+                >
+                <a
+                    href="/dashboard-transactions-details.html"
+                    class="card card-list d-grid gap-2"
+                >
+                    <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-1">
+                        <img
+                            src="/images/dashboard-icon-product-1.png"
+                            alt=""
+                        />
+                        </div>
+
+                        <div class="col-md-4">Shirup Marzzan</div>
+                        <div class="col-md-3">Bagas Satrio</div>
+                        <div class="col-md-3">19 January, 2022</div>
+                        <div class="col-md-1 d-none d-md-block">
+                        <img src="/images/dashboard-arrow-right.svg" />
+                        </div>
+                    </div>
+                    </div>
+                </a>
+                <a
+                    href="/dashboard-transactions-details.html"
+                    class="card card-list d-grid gap-2"
+                >
+                    <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-1">
+                        <img
+                            src="/images/dashboard-icon-product-2.png"
+                            alt=""
+                        />
+                        </div>
+
+                        <div class="col-md-4">LeBrone X</div>
+                        <div class="col-md-3">Masayoshi</div>
+                        <div class="col-md-3">16 January, 2022</div>
+                        <div class="col-md-1 d-none d-md-block">
+                        <img src="/images/dashboard-arrow-right.svg" />
+                        </div>
+                    </div>
+                    </div>
+                </a>
+                <a
+                    href="/dashboard-transactions-details.html"
+                    class="card card-list d-grid gap-2"
+                >
+                    <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-1">
+                        <img
+                            src="/images/dashboard-icon-product-3.png"
+                            alt=""
+                        />
+                        </div>
+
+                        <div class="col-md-4">Soffa Lembutte</div>
+                        <div class="col-md-3">Shayna</div>
+                        <div class="col-md-3">11 January, 2022</div>
+                        <div class="col-md-1 d-none d-md-block">
+                        <img src="/images/dashboard-arrow-right.svg" />
+                        </div>
+                    </div>
+                    </div>
+                </a>
+                </div>
+                <div
+                class="tab-pane fade"
+                id="pills-profile"
+                role="tabpanel"
+                aria-labelledby="pills-profile-tab"
+                >
+                <a
+                    href="/dashboard-transactions-details.html"
+                    class="card card-list d-grid gap-2"
+                >
+                    <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-1">
+                        <img
+                            src="/images/dashboard-icon-product-1.png"
+                            alt=""
+                        />
+                        </div>
+
+                        <div class="col-md-4">Shirup Marzzan</div>
+                        <div class="col-md-3">Bagas Satrio</div>
+                        <div class="col-md-3">19 January, 2022</div>
+                        <div class="col-md-1 d-none d-md-block">
+                        <img src="/images/dashboard-arrow-right.svg" />
+                        </div>
+                    </div>
+                    </div>
+                </a>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DashboardProductController;
+use App\Http\Controllers\DashboardTransactionController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
@@ -26,12 +27,16 @@ Route::get('/details/{id}', [DetailController::class, 'index'])->name('detail');
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::get('/success', [CartController::class, 'success'])->name('success');
 
-Route::get('/register-success', [RegisterController::class, 'success'])->name('register-success');
+Route::get('/register/success', [RegisterController::class, 'success'])->name('register-success');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/dashboard-products', [DashboardProductController::class, 'index'])->name('dashboard-products');
-Route::get('/dashboard-products/create', [DashboardProductController::class, 'create'])->name('dashboard-products-create');
-Route::get('/dashboard-products/{id}', [DashboardProductController::class, 'details'])->name('dashboard-products-details');
+
+Route::get('/dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-products');
+Route::get('/dashboard/products/create', [DashboardProductController::class, 'create'])->name('dashboard-products-create');
+Route::get('/dashboard/products/{id}', [DashboardProductController::class, 'details'])->name('dashboard-products-details');
+
+Route::get('/dashboard/transactions', [DashboardTransactionController::class, 'index'])->name('dashboard-transaction');
+Route::get('/dashboard/transactions/{id}', [DashboardTransactionController::class, 'details'])->name('dashboard-transaction-details');
 
 Auth::routes();
 
