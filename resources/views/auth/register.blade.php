a30f5ba3b05070066f030c41a706f15761d64eee
dandisatrio <dandisatrio810@gmail.com>
2022-01-25 16:43:36 +0700

    convert register dan login page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,87 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+<div class="page-content page-auth" id="register">
+    <div class="section-store-auth" data-aos="fade-up">
+      <div class="container">
+        <div class="row align-items-center justify-content-center row-login">            
+          <div class="col-lg-4">
+            <h2>
+              Memulai untuk jual beli <br />
+              dengan cara terbaru
+            </h2>
+            <form class="mt-3">
+              <div class="form-group mb-2">
+                <label>Full Name</label>
+                <input 
+                  type="email" 
+                  class="form-control is-valid" 
+                  v-model="name" 
+                  autofocus 
+                />
+              </div>
+              <div class="form-group mb-2">
+                <label>Email Address</label>
+                <input type="email" class="form-control is-valid" v-model="email" />
+              </div>
+              <div class="form-group mb-2">
+                <label>Password</label>
+                <input type="password" class="form-control" />
+              </div>
+              <div class="form-group mb-3">
+                <label>Store</label>
+                <p class="text-muted">Apakah anda juga ingin membuka toko?</p>
+                <div class="form-check form-check-inline">
+                  <input 
+                    type="radio" 
+                    class="form-check-input"
+                    name="is_store_open"
+                    id="openStoreTrue"
+                    v-model="is_store_open"
+                    :value="true"
+                  >
+                  <label for="openStoreTrue" class="custom-control-label">Iya, silahkan</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input 
+                    type="radio" 
+                    class="form-check-input"
+                    name="is_store_open"
+                    id="openStoreFalse"
+                    v-model="is_store_open"
+                    :value="false"
+                  >
+                  <label for="openStoreFalse" class="custom-control-label">Belum, terima kasih</label>
+                </div>
+              </div>
+              <div class="form-group mb-2" v-if="is_store_open">
+                <label>Nama Toko</label>
+                <input 
+                  type="text" 
+                  class="form-control"
+                />
+              </div>
+              <div class="form-group" v-if="is_store_open">
+                <label>Kategori</label>
+                <select name="category" class="form-select">
+                  <option value="" disabled>Select Category</option>
+                </select>
+              </div>
+
+              <a href="/dashboard.html" class="btn btn-success d-grid gap-2 mt-4">
+                Sign Up Now
+              </a>
+              <a href="/login.html" class="btn btn-signup d-grid gap-2 mt-2">
+                Back to Sign In
+              </a>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<div class="container" style="display: none">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -75,3 +155,33 @@
     </div>
 </div>
 @endsection
+
+@push('addon-script')
+<script src="/vendor/vue/vue.js"></script>
+    <script src="https://unpkg.com/vue-toasted"></script>
+    <script>
+      Vue.use(Toasted);
+
+      var register = new Vue({
+        el: '#register',
+        mounted() {
+          AOS.init();
+          this.$toasted.error(
+            "Maaf, tampaknya email sudah terdaftar pada sistem kami",
+            {
+              position: "top-center",
+              clasName: "rounded",
+              duration: 1000
+            }
+          );
+        },
+        data: {
+          name: "Bambang Satrio Gandhi",
+          email: "bambang@mail.com",
+          password: "",
+          is_store_open: true,
+          store_name: "",
+        }
+      })
+    </script>
+@endpush
---
+++ resources/views/includes/navbar-auth.blade.php
@@ -0,0 +1,34 @@
+<nav
+      class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top"
+      data-aos="fade-down"
+    >
+      <div class="container">
+        <a href="{{ url('home') }}" class="navbar-brand">
+          <img src="images/logo.svg" alt="logo" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item active">
+              <a href="{{ url('home') }}" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('category') }}" class="nav-link">Category</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Rewards</a>
+            </li>
+          </ul>
+        </div>
+    </div>
+</nav>
